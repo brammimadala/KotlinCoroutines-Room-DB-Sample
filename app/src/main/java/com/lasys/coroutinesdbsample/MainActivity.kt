4f74806468419805e0329205d0321b310b188a27
brammigithub <brahmam@blazeautomation.com>
2021-09-15 20:38:21 +0530

    1.RoomDatabase Testing implemented. 2.Hilt concept implemented.

--- app/src/main/java/com/lasys/coroutinesdbsample/MainActivity.kt
+++ app/src/main/java/com/lasys/coroutinesdbsample/MainActivity.kt
@@ -5,7 +5,6 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.lasys.coroutinesdbsample.adapter.SubscribersAdapter
 import com.lasys.coroutinesdbsample.databinding.ActivityMainBinding
